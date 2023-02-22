6bbcba303cfa71d96e914abb4ac7bb00e7439a9f
Shawn Ryer <Ryers@moravian.edu>
2023-02-22 18:08:10 -0500

    done

--- Sprint01/src/Project01/People.java
+++ Sprint01/src/Project01/People.java
@@ -2,6 +2,14 @@ package Project01;
 
 import Project01.PeopleType;
 
+/**
+ * The People Class
+ * This class uses a constructor to create a specific person. It holds variables for
+ * the specific person, including their nation, tribe, who they are, description, and lifepoints.
+ * It also contains the functions for interacting with the person, including checking of
+ * they're alive, and reducing their health points as needed.
+ */
+
 public class People
 {
     private String personName;
@@ -12,6 +20,15 @@ public class People
     private int myLifePoints;
 
 
+    /**
+     * Constructor for People.
+     * This function takes 4 parameters, nation, tribe, person, lifepoints.
+     * It is important to note that, person is of type PeopleType, created in the PeopleType class.
+     * @param nation The nationality of the specific person
+     * @param tribe The tribe of the specific person
+     * @param person The specific person is designated a soldier or tricky.
+     * @param lifePoints The lifePoints of the specific person
+     */
     public People(String nation, String tribe, PeopleType person, int lifePoints)
     {
         myNation = nation;
@@ -21,30 +38,61 @@ public class People
         myLifePoints = lifePoints;
     }
 
+    /**
+     * Function to get PeopleType.
+     * @return the type of said person
+     */
+
     public PeopleType getType()
     {
         return me;
     }
+
+    /**
+     * Function to get the tribe of a person.
+     * @return the tribe type of said person
+     */
 
     public String getTribe()
     {
         return myTribe;
     }
 
+    /**
+     * Function to get the nation of a person.
+     * @return the nation type of said person
+     */
+
     public String getNation()
     {
         return myNation;
     }
+    /**
+     * Function to check to see if the persons health is at least greater than 0.
+     * @return the health of the person above 0
+     */
 
     public Boolean isPersonAlive()
     {
         return (myLifePoints > 0);
     }
 
+    /**
+     * Function to get the health of the person.
+     * @return the health of the person
+     */
     public int getLifePoints()
     {
         return myLifePoints;
     }
+
+    /**
+     * This is the encounterStrategy
+     * This function takes 1 parameter; encounterStrategy
+     * @param otherPerson otherPerson is determined based on an interaction with
+     * a member of another party. This will help determine what happens during the interaction
+     * and accordingly solve the action at hand.
+     */
 
     public void encounterStrategy(People otherPerson)
     {
@@ -61,12 +109,20 @@ public class People
         }
     }
 
+    /**
+     * Function to update the health of the person
+     */
 
     public void reduceLifePoints(int points)
     {
         myLifePoints = myLifePoints - points;
     }
 
+
+    /**
+     * Function to display all the information about a person and have it formatted.
+     * @return the toString, to create formatting.
+     */
 
     public String toString()
     {
--- Sprint01/src/Project01/Tribe.java
+++ Sprint01/src/Project01/Tribe.java
@@ -8,11 +8,20 @@ import Project01.PeopleType;
 
 public class Tribe
 {
-    private String nationName;
-    private String tribeName;
-    private int tribeLifePoints;
-    private ArrayList<People> members = new ArrayList<>();
-    private ArrayList<People> livingMembers = new ArrayList<>();
+    private String nationName; // Creates just the string of nation names for individual nations
+    private String tribeName; // Creates just the string of nation names for tribe nations
+    private int tribeLifePoints; // This is the unique int for this class to calculate life for a specific tribe
+    private ArrayList<People> members = new ArrayList<>(); // This array is strictly used for containing members within the class
+    private ArrayList<People> livingMembers = new ArrayList<>(); // This is an array used for storing strictly living entities.
+
+
+    /**
+     * Constructor for Tribe.
+     * This function takes 3 parameters, nation, tribe, and lifepoints.
+     * @param nation The nationality of the specific tribe
+     * @param tribe The tribe name
+     * @param lifePoints The lifePoints of the specific tribe
+     */
 
     public Tribe(String nation, String tribe, int lifePoints)
     {
@@ -23,10 +32,15 @@ public class Tribe
             if(i % 2 == 0)
                 members.add(new People(nationName, tribeName, PeopleType.warrior, tribeLifePoints / 2));
             else
-                members.add(new People(nationName, tribeName, PeopleType.wizzard, tribeLifePoints / 2));
+                members.add(new People(nationName, tribeName, PeopleType.wizard, tribeLifePoints / 2));
         for(int i = 0; i < members.size(); i++)
             livingMembers.addAll(members);
     }
+
+    /**
+     * Function for collecting all living tribe members
+     * Returns an arraylist of living entities
+     */
 
     public ArrayList<People> getLivingTribeMembers()
     {
@@ -44,37 +58,60 @@ public class Tribe
         //System.out.println(livingMembers);
         return livingMembers;
     }
-    /*
-    public void printMembers()
-    {
-        for(int i = 0; i < 2; i++)
-        {
-            System.out.println(people.get(i));
-        }
-    }
+   /*
+   public void printMembers()
+   {
+       for(int i = 0; i < 2; i++)
+       {
+           System.out.println(people.get(i));
+       }
+   }
 */
 
+    /**
+     * Function to get Tribe size.
+     * @return the type of said person
+     */
 
     public int getTribeSize()
     {
         return livingMembers.size();
     }
 
+    /**
+     * Function to determine if tribe health is above 0.
+     * @return a boolean value if above 0
+     */
+
     public Boolean isTribeAlive()
     {
         return (tribeLifePoints > 0);
     }
 
+    /**
+     * Function to get tribe life points
+     * @return an int of said tribe life points
+     */
 
     public int getTribeLifePoints()
     {
         return tribeLifePoints;
     }
 
+    /**
+     * Function to get the tribes name
+     * @return a string of the tribe name
+     */
+
     public String getTribeName()
     {
         return tribeName;
     }
+
+    /**
+     * Function to display all the information members and results about encounters.
+     * @return the toString, to create formatting.
+     */
 
     public String toString()
     {
